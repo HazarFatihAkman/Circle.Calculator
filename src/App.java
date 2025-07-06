37ba1b2bca4b0398fe3faeb5934b6bee1153d45e
Hazar Fatih Akman <hazarfatihakman@Hazar-MacBook-Pro.local>
2025-07-07 02:58:40 +0300

    completed

---
+++ src/App.java
@@ -0,0 +1,35 @@
+import java.util.Objects;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        double r, pi = 3.14, alfa, area, perimeter;
+        boolean exit = false;
+        Scanner sc = new Scanner(System.in);
+
+        while (!exit) {
+            r = -1;
+            alfa = -1;
+
+            while (r < 0) { 
+                System.out.print("| Enter circle radius : ");
+                r = sc.nextDouble();
+            }
+
+            area = pi * r * r;
+            perimeter = 2 * pi * r;
+            System.out.println("| Area : " + area);
+            System.out.println("| Perimeter : " + perimeter);
+
+            while (alfa < 0 || alfa > 360) {
+                System.out.print("| Center angle : ");
+                alfa = sc.nextDouble();
+            }
+
+            System.out.println("| Area of Circle : " + (pi * r * r * alfa) / 360 + "\n------");
+            System.out.print("| Exit (Y/N) : ");
+            exit = Objects.equals(sc.next(), "Y");
+        }
+
+    }
+}
